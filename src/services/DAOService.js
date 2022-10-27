b457d65534a6089321ff0d9a6407fecf32c5fa47
Gonzalo Cabrera <cabrerafernandogonzalo@gmail.com>
2022-10-27 08:06:41 -0300

    Agrega funciones DAO readData, get...ByID

--- src/services/DAOService.js
+++ src/services/DAOService.js
@@ -5,4 +5,36 @@ export default class DAOService{
   constructor(){
 
   }
+
+  readData (entityType){
+    return JSON.parse(fs.readFileSync(process.cwd()+'/src/data/'+entityType+'.json', 'utf-8'))  
+  }
+
+  saveEntity (entityType, entity){
+    const entities = this.readData(entityType)
+    entities.push(entity)
+    try {
+      fs.writeFileSync(process.cwd()+'/src/data/'+entityType+'.json', JSON.stringify(entities))
+      console.log('Entities saved.')
+    }catch (error){
+      console.error (error)
+    }
+  }
+
+  getDistributorByID(id){
+    const result = this.readData('distributors').filter(distributor => distributor.id == id)    
+    return result.length == 0 ? null : new Entity(result[0].id, result[0].name, result[0].keypair)
+  }
+
+  getAplicatorByID(id){
+    const result = this.readData('aplicators').filter(aplicator => aplicator.id == id)    
+    return result.length == 0 ? null : new Entity(result[0].id, result[0].name, result[0].keypair)    
+  }
+
+  getReceiverByID(id){
+    const result = this.readData('citizens').filter(citizen => citizen.id == id)    
+    return result.length == 0 ? null : new Entity(result[0].id, result[0].name, result[0].keypair)    
+  }
+
 }
+
