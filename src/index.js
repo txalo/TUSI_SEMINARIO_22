fdd2ac13cea2b1ab8cb2a9f4f032566a25a050a0
Gonzalo Cabrera <cabrerafernandogonzalo@gmail.com>
2022-12-01 17:22:34 -0300

    Instala swagger UI para documentacion, se agregan importaciones al index

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import express from 'express'
+import { createRequire } from "module";
+
 
 import dotenv from 'dotenv'
 dotenv.config({path:'./src/.env'})
+
+import swaggerUI from 'swagger-ui-express'
 
 import issuerRouter from './routes/issuerRouter.js'
 import distributorRouter from './routes/distributorRouter.js'
 import applicatorRouter from './routes/applicatorRouter.js'
 import receiverRouter from './routes/receiverRouter.js'
 
+const require = createRequire(import.meta.url);
 
 const vac = express()
+const swaggerDocument = require('./data/documentation.json')
 
 vac.use(express.json())
 vac.use(express.urlencoded({extended: true}))
@@ -22,6 +28,7 @@ vac.use(distributorRouter)
 vac.use(applicatorRouter)
 vac.use(receiverRouter)
 
+vac.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 vac.listen(process.env.PORT, () => console.log( `Listen at port ${process.env.PORT}` ))
 
 
